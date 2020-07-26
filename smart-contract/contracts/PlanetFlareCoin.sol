e7043779e3ab9bb1a00201d498b6fd3a760a97f8
Jashan Shewakramani <jashan1998s@gmail.com>
2020-07-25 21:57:41 -0400

    API to fetch all bounties for a given publisher

--- smart-contract/contracts/PlanetFlareCoin.sol
+++ smart-contract/contracts/PlanetFlareCoin.sol
@@ -27,6 +27,7 @@ contract PlanetFlareCoin {
     mapping (address => mapping (address => uint256)) allowances;
 
     /* Bounty Variables */
+    mapping(address => uint256[]) bountyIDsByPublisher;
     mapping (uint256 => Bounty) bountiesByID;
 
     /* Constructor */
@@ -90,6 +91,10 @@ contract PlanetFlareCoin {
         return (bounty.id, bounty.publisher, bounty.contentID, bounty.deposit);
     }
 
+    function getBountiesForPublisher(address publisher) public view returns (uint256[] memory) {
+        return bountyIDsByPublisher[publisher];
+    }
+
     function createBounty(string memory contentID, uint256 deposit) public returns (uint256 id) {
         require (deposit <= balances[msg.sender], "Not enough balance");
 
@@ -105,6 +110,9 @@ contract PlanetFlareCoin {
         bounty.lastUpdated = now;
 
         // TODO: append to publisher list
+        uint256[] storage publisherBounties = bountyIDsByPublisher[msg.sender];
+        publisherBounties.push(bountyID);
+        
         emit BountyUpdate(bountyID, msg.sender, contentID, deposit);
 
         return bountyID;
@@ -135,14 +143,24 @@ contract PlanetFlareCoin {
     function deleteBounty(uint256 bountyID) public returns (bool success) {
         Bounty storage bounty = bountiesByID[bountyID];
 
+        // check permissions
         require(bounty.publisher == msg.sender, "Unauthorized delete, or bounty does not exist");
 
-        balances[msg.sender] += bounty.deposit;
-        bounty.deposit = 0;
-
+        // emit event indicating deletion and then delete
         emit BountyUpdate(bountyID, msg.sender, bounty.contentID, 0);
 
-        bounty.publisher = address(0); // reset, so that creation can be done again
+        // delete it from the main list
+        delete bountiesByID[bountyID];
+
+        // delete from publisher list by creating a new one (thank the lack of a std lib for the shitty complexity)
+        uint256[] memory oldBountyList = bountyIDsByPublisher[msg.sender];
+        delete bountyIDsByPublisher[msg.sender];
+        uint256[] storage newBountyList = bountyIDsByPublisher[msg.sender];
+
+        for (uint i = 0; i < oldBountyList.length; ++i) {
+            if (oldBountyList[i] != bountyID)
+                newBountyList.push(oldBountyList[i]);
+        }
 
         return true;
     }
